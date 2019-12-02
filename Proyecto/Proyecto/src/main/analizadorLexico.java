925097c83105f25ed8c40618bca41e1ab2263de4
edgar <edgar9722@gmail.com>
2019-12-02 14:58:39 -0600

    holis

--- Proyecto/Proyecto/src/main/analizadorLexico.java
+++ Proyecto/Proyecto/src/main/analizadorLexico.java
@@ -13,166 +13,181 @@ public class analizadorLexico {
     DataInputStream in;
     String bufferIn, cad;
     int i;
-    
-    public analizadorLexico(String f) throws FileNotFoundException{
+
+    public analizadorLexico(String f) throws FileNotFoundException {
         programa(f);
     }
-    
-    public void programa(String f) throws FileNotFoundException{
-        in = new DataInputStream(new FileInputStream(f));//leemos nuestro archivo de entrada
+
+    public void programa(String f) throws FileNotFoundException {
+        in = new DataInputStream(new FileInputStream(f));// leemos nuestro archivo de entrada
         try {
-            if ((bufferIn = in.readLine()) != null) {//Leemos la primera linea de entrada que tiene que ser 'inicio'
-                cad = bufferIn.trim();//Esto seria el equivalente a transformarlo a String
-                if (cad.equals("inicio")){
+            if ((bufferIn = in.readLine()) != null) {// Leemos la primera linea de entrada que tiene que ser 'inicio'
+                cad = bufferIn.trim();// Esto seria el equivalente a transformarlo a String
+                if (cad.equals("inicio")) {
                     bufferIn = in.readLine();
                     cad = bufferIn.trim();
-                    if (cad.equals("fin"))//Verificamos que no haya terminado
+                    if (cad.equals("fin"))// Verificamos que no haya terminado
                         System.out.println("Termino el programa");
                     else
                         declaracion_sentencia();
-                }else{
-                    //No comenzo con 'inicio'
+                } else {
+                    // No comenzo con 'inicio'
                 }
             }
-        }catch(IOException io){
-            
+        } catch (IOException io) {
+
         }
-    }
-    
-    public void declaracion_sentencia(){
-        i=0; String aux="";
-        while(Character.isLetter(cad.charAt(i))){
-            aux+=cad.charAt(i);
-            i++;
-        }//La i termina en la posicion del caracter que no es una letra
-        switch(aux){
-            case "if":
-                break;
-            case "do":
-                break;
-            case "while":
-                break;
-            case "for":
-                if(cad.charAt(i)=='('){
-                     aux+=cad.charAt(i);
-                        i++;
-                    inicializacion();
-                    
-                }
-                
-                 
-                break;
-            default:
-        }
-    }
-    
-    public String operador_logico(){
-        String aux="";
-        while(cad.charAt(i) == '/' || cad.charAt(i) == '*' || cad.charAt(i) == '+' || cad.charAt(i) == '-'){
-            aux+=cad.charAt(i);
-            i++;
-        }
-        return aux;
-    }
-    public boolean identificador(){
-        return false;
     }
 
-    public boolean letra(){
-        boolean aux=false;
+    public void declaracion_sentencia() {
+        i = 0;
+        String aux = "";
+        while (Character.isLetter(cad.charAt(i))) {
+            aux += cad.charAt(i);
+            i++;
+        } // La i termina en la posicion del caracter que no es una letra
+        switch (aux) {
+        case "if":
+            break;
+        case "do":
+            break;
+        case "while":
+            break;
+        case "for":
+            if (cad.charAt(i) == '(') {
+                aux += cad.charAt(i);
+                i++;
+                inicializacion();
+
+            }
+
+            break;
+        default:
+        }
+    }
+
+    public String operador_logico() {
+        String aux = "";
+        while (cad.charAt(i) == '/' || cad.charAt(i) == '*' || cad.charAt(i) == '+' || cad.charAt(i) == '-') {
+            aux += cad.charAt(i);
+            i++;
+        }
+        return aux;
+    }
+
+    public boolean identificador() {
+        boolean bandera = false;
+        while (letra()) {
+            aux += cad.charAt(i);
+            i++;
+            while (num_entero()) {
+                aux += cad.charAt(i);
+                i++;
+            }
+            bandera = true;
+        }
+    }
+
+    }
+
+    public boolean letra() {
+        boolean aux = false;
         if (Character.isLetter(cad.charAt(i)))
-            aux=true;
-        i++;
+            aux = true;
+
         return aux;
     }
-    
-    public boolean num_entero(){
-        boolean aux=false;
-        if(Character.isDigit(cad.charAt(i)))
-            aux=true;
-        i++;
+
+    public boolean num_entero() {
+        boolean aux = false;
+        if (Character.isDigit(cad.charAt(i)))
+            aux = true;
         return aux;
     }
-    
+
     public 
     public declaracion_var(){
         
     }
+
     public expresion(){
         
     }
-    public String operador(){
 
-        String aux="";
-        while(cad.chatAt(i) == '/' || '*' || '+' || '-'){
-            aux+=cad.charAt(i);
-            i++;
+    public boolean operador() {
+
+        if (cad.chatAt(i) == '/' || '*' || '+' || '-') {
+            return true;
         }
-        return aux;
+        i++;
+        return false;
     }
-    
-    public String inicializacion(){
-        String aux="";
-        if(cad.charAt(i)== 'i'){
-            aux+=cad.charAt(i);
+
+    public String inicializacion() {
+        String aux = "";
+        if (cad.charAt(i) == 'i') {
+            aux += cad.charAt(i);
             i++;
-            if(cad.charAt(i)== 'n'){
-                aux+=cad.charAt(i);
+            if (cad.charAt(i) == 'n') {
+                aux += cad.charAt(i);
                 i++;
-                if(cad.charAt(i)== 't'){
-                    aux+=cad.charAt(i);
+                if (cad.charAt(i) == 't') {
+                    aux += cad.charAt(i);
                     i++;
                     identificador();
-                
-                }else
+
+                } else
                     error();
-            }else
+            } else
                 error();
- 
-        } else if (identificador()){
-            aux+=cad.charAt(i);
+
+        } else if (identificador()) {
+            aux += cad.charAt(i);
             i++;
-            if(cad.charAt(i)== '='){
-                aux+=cad.charAt(i);
+            if (cad.charAt(i) == '=') {
+                aux += cad.charAt(i);
                 i++;
-                
-                while(Character.isDigit(cad.charAt(i))){
-                    aux+=cad.charAt(i);
+
+                while (Character.isDigit(cad.charAt(i))) {
+                    aux += cad.charAt(i);
                     i++;
-                    }
-            }else{
+                }
+            } else {
                 error();
             }
-        }else{
+        } else {
             error();
         }
         return aux;
     }
-    
-    public String incremento(){
-        
-         String aux="";
-         if(cad.charAt(i)== '+'){
-            aux+=cad.charAt(i);
+
+    public String incremento() {
+
+        String aux = "";
+        if (cad.charAt(i) == '+') {
+            aux += cad.charAt(i);
             i++;
-            if(cad.charAt(i)== '+'){
-                aux+=cad.charAt(i);
+            if (cad.charAt(i) == '+') {
+                aux += cad.charAt(i);
                 i++;
-            }else{
+            } else {
                 error();
             }
-         }else if(cad.charAt(i)== '-'){
-                aux+=cad.charAt(i);
+        } else if (cad.charAt(i) == '-') {
+            aux += cad.charAt(i);
+            i++;
+            if (cad.charAt(i) == '-') {
+                aux += cad.charAt(i);
                 i++;
-                    if(cad.charAt(i)== '-'){
-                    aux+=cad.charAt(i);
-                    i++;
-                    }else{
-                    error();
-                    }
-                }else{
+            } else {
                 error();
-         }
-      return aux;   
+            }
+        } else {
+            error();
+        }
+        return aux;
     }
+
+    public
+
 }
